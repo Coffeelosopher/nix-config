ab75b698ddb0de9f21967d6bf8ec8658bd4776cf
wsl default user <nixos@nixos.>
2025-04-24 19:28:40 +0200

    cleaned up

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -19,35 +19,18 @@
     ../../modules/software/packages/azure.nix
     
     # Editors
-    ## Emacs
-    #../../modules/software/editors/emacs.nix
-
     ## Neovim
     ../../modules/software/editors/neovim/nvchad.nix
-
-    ## Desktop
-    #../../modules/software/desktop/gnome.nix
 
     ## Container
     #../../modules/software/virt/container/docker/default.nix
     #../../modules/software/virt/container/docker/watchtower.nix
     #../../modules/software/virt/container/lxd/default.nix
 
-    # Services
-     # Default
-    #../../modules/default/default.nix
 
     # Housekeeping
     ## garbage collection
     #../../modules/houskeeping/gc/default.nix
-
-
-    ## VPN
-    #../../modules/software/vpn/tailscale/default.nix
-    #../modules/software/vpn/zerotier/default.nix
-
-    ## Gaming
-    #../../modules/software/gaming/steam.nix
 
   ];
 
