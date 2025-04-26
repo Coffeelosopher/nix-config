dacf71ba48e744f988ee72e71d40df1237182652
Nico Nicklis <nico.nicklis@icloud.com>
2025-04-26 22:40:34 +0200

    cleaned imports

--- bundles/default.nix
+++ bundles/default.nix
@@ -1,27 +1,19 @@
 { config, pkgs, ... }: {
   imports = [
-    # Default
-    #../modules/default/default.nix
+    # Base
     ../modules/base/server.nix
-
-    # Housekeeping
-    #../modules/houskeeping/alias/nixos.nix
-    ../modules/houskeeping/gc/default.nix
-    #../modules/houskeeping/scripts/default.nix
 
     # Software
     ## Packages
     ../modules/software/packages/default.nix
+
     ## Shell
     ../modules/software/shell/zsh.nix
 
     ## VPN
     ../modules/software/vpn/tailscale/default.nix
 
-    # Hardware
-    #../modules/hardware/hdd-sleep.nix
-    
-    ## Storage
-    #../modules/hardware/storage/portable.nix
+    # Housekeeping
+    ../modules/houskeeping/gc/default.nix
   ];
 }
