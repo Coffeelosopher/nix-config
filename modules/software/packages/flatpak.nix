ba04350323b61de9ac1fa232f56be64fbcf25e67
Nico Nicklis <nico.nicklis@icloud.com>
2025-04-28 22:38:20 +0200

    changed software center, removed flatpak hub setup service

--- modules/software/packages/flatpak.nix
+++ modules/software/packages/flatpak.nix
@@ -4,15 +4,16 @@
   users.users.${userName}= {
     packages = with pkgs; [
       flatpak
-      gnome-software
+      #gnome-software
+      kdePackages.discover
     ];
   };
 
-  systemd.services.configure-flathub-repo = {
-    wantedBy = [ "default.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+  #systemd.services.configure-flathub-repo = {
+  #  wantedBy = [ "default.target" ];
+  #  path = [ pkgs.flatpak ];
+  #  script = ''
+  #    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #  '';
+  #};
 }
