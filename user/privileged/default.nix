1dd757e128ba35ee4e9d6411a1d1aaa0462a9ac9
Nico Nicklis <nico.nicklis@icloud.com>
2025-05-07 18:24:54 +0200

    add key to user

--- user/privileged/default.nix
+++ user/privileged/default.nix
@@ -10,4 +10,8 @@
     extraGroups =
     [ "networkmanager" "network" "wheel" "lxd" "docker" "libvirtd" "syncthing" ];
   };
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRamh4k5eDwjYopD889LnxlEJVFsjJuOeTxvbMSZMMK nico.nicklis@icloud.com"
+  ];
 }
