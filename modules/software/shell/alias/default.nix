14ea65337709bccbf9257a13c3b7ec0ddc91166f
Nico Nicklis <nico.nicklis@icloud.com>
2025-05-07 19:01:12 +0200

    added test

--- modules/software/shell/alias/default.nix
+++ modules/software/shell/alias/default.nix
@@ -5,6 +5,7 @@
     nix          = "nix --extra-experimental-features ' nix-command flakes ' ";
     flake-update = "nix flake update --commit-lock-file && git push";
     nixos-rebuild-flake = "nixos-rebuild switch --flake 'github:coffeelosopher/nix-config#${variables.hostname}' --refresh";
+    testung = "echo '${variables.hostname}'";
 
     # SSH Config Stuff
     jssh     = "ssh -J hub";
