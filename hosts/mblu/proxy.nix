1fb1f2f70a956564fed149bf63b515348762bc59
Nico Nicklis <nico.nicklis@icloud.com>
2025-05-08 19:41:41 +0200

    update

--- hosts/mblu/proxy.nix
+++ hosts/mblu/proxy.nix
@@ -61,6 +61,20 @@
      # }
      '';
 
+    virtualHosts."db.nicklis.cloud" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:8080";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;";
+      };
+    };
+
     virtualHosts."own.nicklis.cloud" = {
       enableACME = true;
       forceSSL = true;
